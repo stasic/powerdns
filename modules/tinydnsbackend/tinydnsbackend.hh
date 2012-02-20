c41372d3e438967f81c3f62b6d24e045f7375977
Ruben d'Arco <cyclops@prof-x.net>
2012-02-20 23:09:13 +0100

    TinyDNS Backend master mode - feature complete
    This gives the tinydnsbackend master mode capabilities.
    
    query ALL the records!

--- modules/tinydnsbackend/tinydnsbackend.hh
+++ modules/tinydnsbackend/tinydnsbackend.hh
@@ -27,6 +27,7 @@ public:
 	void setNotified(uint32_t id, uint32_t serial);
 private:
 	vector<string> getLocations();
+	vector<DomainInfo> getDomainInfo();
 
 	//data member variables
 	uint64_t d_taiepoch;
@@ -35,6 +36,9 @@ private:
 	DNSPacket *d_dnspacket; // used for location and edns-client support.
 	bool d_isWildcardQuery; // Indicate if the query received was a wildcard query.
 	bool d_isAxfr; // Indicate if we received a list() and not a lookup().
+	
+
+	// Statics
 	static pthread_mutex_t s_domainInfoLock;
 	static vector<DomainInfo> s_domainInfo;
 };
