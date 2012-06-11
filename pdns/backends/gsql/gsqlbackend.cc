8ea7c2ff6f64cd47c2614784891b4e6a0a74400f
Peter van Dijk <peter.van.dijk@netherlabs.nl>
2012-06-11 14:53:45 +0200

    fix error message in GSQLBackend::isMaster

--- pdns/backends/gsql/gsqlbackend.cc
+++ pdns/backends/gsql/gsqlbackend.cc
@@ -89,7 +89,7 @@ bool GSQLBackend::isMaster(const string &domain, const string &ip)
     d_db->doQuery(output, d_result);
   }
   catch (SSqlException &e) {
-    throw AhuException("GSQLBackend unable to retrieve list of master domains: "+e.txtReason());
+    throw AhuException("GSQLBackend unable to check master status of domain "+domain+": "+e.txtReason());
   }
 
   if(d_result.empty())
